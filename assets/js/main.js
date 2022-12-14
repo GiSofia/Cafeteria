d919f8b235d08e542bf922c067407b74930dfc04
Giuliana Saborío <giulianasaborio@gmail.com>
2022-12-13 21:44:21 -0600

    Create of header and logo and configure button, layout

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/*===== SHOW MENU =======*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
