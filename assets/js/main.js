2b9fa7e1da98c2c9d63430e44e1284d266d1bb2b
Giuliana Saborío <giulianasaborio@gmail.com>
2022-12-17 23:55:59 -0600

    Adding new images for section products filter and creating of this

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,26 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
+/*==================== MIXITUP FILTER PRODUCTS ====================*/
+var mixer = mixitup('.products__content', {
+    selectors: {
+        target: '.products__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/*===== Default filter products =======*/
+mixerProducts.filter('.all')
+
+/*===== Link active products =======*/
+const linkProducts = document.querySelectorAll('.products__item')
+
+function activeProducts(){
+    linkProducts.forEach(l=> l.classList.remove('active-product'))
+    this.classList.add('active-product')
+}
+
+linkProducts.forEach(l=> l.addEventListener('click', activeProducts))
+
